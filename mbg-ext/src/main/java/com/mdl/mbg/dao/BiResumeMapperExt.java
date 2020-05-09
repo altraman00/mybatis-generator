76a0d957da2fe63fad3bd2e0407afc7993498455
谢昆 <xiekun@sunlands.com>
2020-05-09 15:38:16 +0800

    在生成的mapper.xml中写自定义的sql

--- mbg-ext/src/main/java/com/mdl/mbg/dao/BiResumeMapperExt.java
+++ mbg-ext/src/main/java/com/mdl/mbg/dao/BiResumeMapperExt.java
@@ -1,8 +1,14 @@
 package com.mdl.mbg.dao;
 
 import com.mdl.mbg.mapper.BiResumeMapper;
+import com.mdl.mbg.model.BiResumeEntity;
+
 import javax.annotation.Resource;
+import java.util.List;
 
 @Resource
 public interface BiResumeMapperExt extends BiResumeMapper {
+
+    List<BiResumeEntity> findByName(String jobSeekerName);
+
 }
